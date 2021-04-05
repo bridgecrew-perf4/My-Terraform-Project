75ca8b5082f189a1afe6724c69190aac693828b0
Chad <bartel.chad@gmail.com>
2021-04-05 08:59:45 -0700

    add ec2 output, userdata template file

--- main.tf
+++ main.tf
@@ -8,10 +8,15 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
+  profile = "sso_poweruser"
   region  = "us-west-2"
 }
 
 module "ec2" {
   source = "./modules/ec2"
+}
+
+output "instance_ip_addr" {
+  description = "The private IP address of the main server instance."
+  value       = aws_instance.example.private_ip
 }
