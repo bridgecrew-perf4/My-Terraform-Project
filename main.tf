7abbbeba3bab1c32ea02b0ce678aa31a308a49af
Chad <bartel.chad@gmail.com>
2021-04-04 19:53:26 -0700

    update ec2 module, add outputs

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+}
