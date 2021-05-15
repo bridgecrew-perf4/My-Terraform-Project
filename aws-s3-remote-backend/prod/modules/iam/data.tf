3240e6f4a35e18a5fdaf0a0a012ba4f87a9f249d
chadbartel <bartel.chad@gmail.com>
2021-05-15 07:31:35 -0700

    copy files to prod

---
+++ aws-s3-remote-backend/prod/modules/iam/data.tf
@@ -0,0 +1,81 @@
+/*
+Data used by the IAM Terraform module.
+*/
+
+# S3 bucket data
+data "aws_s3_bucket" "selected" {
+  bucket = var.s3_bucket
+}
+
+# KMS key data
+data "aws_kms_key" "selected" {
+  key_id = var.kms_key
+}
+
+# DynamoDB lock table
+data "aws_dynamodb_table" "selected" {
+  name = var.dynamodb_table
+}
+
+# Terraform IAM policy document
+data "aws_iam_policy_document" "terraform_document" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [
+      data.aws_s3_bucket.selected.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "s3:GetObject",
+      "s3:GetObjectAcl",
+      "s3:GetObjectVersion",
+      "s3:PutObject",
+      "s3:PutObjectAcl",
+      "s3:PutObjectVersionAcl"
+    ]
+    resources = [
+      data.aws_s3_bucket.selected.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "dynamodb:GetItem",
+      "dynamodb:PutItem",
+      "dynamodb:DeleteItem"
+    ]
+    resources = [
+      data.aws_dynamodb_table.selected.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:ListKeys"
+    ]
+    resources = [
+      "*"
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:DescribeKey",
+      "kms:GenerateDataKey"
+    ]
+    resources = [
+      data.aws_kms_key.selected.arn
+    ]
+  }
+}
---
+++ aws-s3-remote-backend/prod/modules/s3/variables.tf
@@ -0,0 +1,27 @@
+/*
+Variables used with the S3 Terraform template.
+*/
+
+variable "environment" {
+  type        = string
+  default     = null
+  description = "Deployment environment name"
+}
+
+variable "region" {
+  type        = string
+  default     = null
+  description = "AWS region"
+}
+
+variable "s3_bucket" {
+  type        = string
+  default     = null
+  description = "Name of the S3 bucket"
+}
+
+variable "bucket_key" {
+  type        = string
+  default     = null
+  description = "Path to the state file inside the S3 bucket. When using a non-default workspace, the state path will be /workspace_key_prefix/workspace_name/key."
+}
