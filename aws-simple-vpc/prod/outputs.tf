046c5d119ecb9db074ac81f6448f6f4b4bc39e66
chadbartel <bartel.chad@gmail.com>
2021-05-17 19:33:57 -0700

    format tf files

--- aws-simple-vpc/prod/outputs.tf
+++ aws-simple-vpc/prod/outputs.tf
@@ -10,12 +10,12 @@ output "aws_vpc_id" {
 
 # Output all public subnets
 output "public_subnets" {
-  value = module.vpc.public_subnets
+  value       = module.vpc.public_subnets
   description = "AWS ID of public subnets in the VPC"
 }
 
 # Output all private subnets
 output "private_subnets" {
-  value = module.vpc.private_subnets
+  value       = module.vpc.private_subnets
   description = "AWS ID of private subnets in the VPC"
 }
